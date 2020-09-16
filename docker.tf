8db1484b7e940384eade0010b9a469a8b15ff25e
Xander Flood <xander.t.flood@gmail.com>
2020-09-16 14:08:57 -0400

    bump to build 6

--- docker.tf
+++ docker.tf
@@ -1,6 +1,6 @@
 resource "docker_container" "payments" {
   name    = "payments"
-  image   = "xanderflood/payment-scraper:build-5"
+  image   = "xanderflood/payment-scraper:build-6"
   command = ["proton"]
   env     = local.env_strings
 
